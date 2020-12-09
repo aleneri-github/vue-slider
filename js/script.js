7a9b95c548667e71d7ffba0061486c2e63784634
Alessandro <aleneri242@gmail.com>
2020-12-09 16:14:36 +0100

    aggiornamento

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 var app = new Vue({
   el: '#root',
   data: {
-    
+    images: [
+      "https://www.giornaledellavela.com/news/wp-content/uploads/2018/01/borghi.jpg",
+      "https://gabrielecaramellino.nova100.ilsole24ore.com/wp-content/uploads/sites/64/2017/06/borghi-italiani.jpg",
+      "https://www.stile.it/wp-content/uploads/2019/06/borghi-italiani.jpg",
+      "https://www.paultaylor.it/wp-content/uploads/2019/03/centro-2.jpg",
+      "https://www.radiomontecarlo.net/resizer/1200/720/true/2020-05-22_16_29_00_panorama-1590157587354-jpg-1590157766454.jpg--i_meravigliosi_borghi_italiani_sul_mare_da_riscoprire_quest_estate.jpg?1590157766000"
+    ]
+
 
   }
 });
